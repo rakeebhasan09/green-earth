4143efb2303e45ad6189f3bf4dc441a5d02b8ad6
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-09-08 00:23:37 +0600

    Add to cart, Delete from cart and price calculation funtionality added.

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const getDate = new Date();
 const fullYear = getDate.getFullYear();
 document.getElementById("footer-year").innerText = fullYear;
 
+const myCart = document.getElementById("cart-items");
+
 // Load Categories
 fetch("https://openapi.programming-hero.com/api/categories")
 	.then((categoriesRes) => categoriesRes.json())
@@ -62,8 +64,11 @@ const loadPlantByCategory = (id) => {
 	if (id === 0) {
 		fetch(`https://openapi.programming-hero.com/api/plants`)
 			.then((plantsRes) => plantsRes.json())
-			.then((plantsData) => displayPlantsByCategory(plantsData.plants));
-
+			.then((plantsData) => {
+				const totalPlants = plantsData.plants;
+				const limitedPlants = totalPlants.slice(0, 6);
+				displayPlantsByCategory(limitedPlants);
+			});
 		return;
 	}
 	fetch(`https://openapi.programming-hero.com/api/category/${id}`)
@@ -80,7 +85,8 @@ const displayPlantsByCategory = (plants) => {
 		plantCard.innerHTML = `
 			<!-- Single Card -->
 			<div
-				class="p-4 bg-white rounded-lg flex flex-col gap-y-3 h-full"
+				id="plant${plant.id}"
+				class="product-cart p-4 bg-white rounded-lg flex flex-col gap-y-3 h-full"
 			>
 				<!-- Image -->
 				<div class="h-[190px]">
@@ -117,11 +123,11 @@ const displayPlantsByCategory = (plants) => {
 						<span
 							class="text-[#1F2937] text-[14px] font-semibold"
 						>
-							৳${plant.price}
+							৳ <span>${plant.price}</span>
 						</span>
 					</p>
 					<button
-						class="w-full mt-3 py-3 px-5 text-white text-[16px] font-medium bg-[#15803D] rounded-full"
+						class="add-to-cart w-full mt-3 py-3 px-5 text-white text-[16px] font-medium bg-[#15803D] rounded-full"
 					>
 						Add to Cart
 					</button>
@@ -130,6 +136,82 @@ const displayPlantsByCategory = (plants) => {
 		`;
 		allPlantsParent.appendChild(plantCard);
 	}
+};
+
+// Add To Cart Handler
+let cartProducts = [];
+document.getElementById("all-plants-parent").addEventListener("click", (e) => {
+	if (e.target.innerText === "Add to Cart") {
+		handleAddToCart(e);
+	}
+});
+
+const handleAddToCart = (e) => {
+	const card = e.target.closest(".product-cart");
+	const cardTitle = card.querySelector("h4").innerText;
+	const plantID = e.target.parentNode.parentNode.id;
+	const price = Number(
+		e.target.parentNode.children[0].children[1].children[0].innerText
+	);
+	cartProducts.push({
+		id: plantID,
+		title: cardTitle,
+		price: price,
+	});
+	showCartItems(cartProducts);
+};
+
+// Show Cart Items
+const showCartItems = (cartProducts) => {
+	let total = 0;
+	myCart.innerHTML = "";
+	cartProducts.forEach((cartProduct) => {
+		myCart.innerHTML += `
+			<div
+				class="py-2 px-3 bg-[#F0FDF4] rounded-lg flex items-center justify-between"
+			>
+				<div>
+					<h5
+						class="text-[#1F2937] text-[14px] font-semibold"
+					>
+						${cartProduct.title}
+					</h5>
+					<p
+						class="text-[#1F2937] text-[16px] font-normal"
+					>
+						৳ <span>${cartProduct.price}</span>
+					</p>
+				</div>
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="30"
+						height="30"
+						viewBox="0 0 16 16"
+						fill="#f00"
+						class="cursor-pointer"
+						onclick="handleDeleteFromCart('${cartProduct.id}')"
+					>
+						<path
+							d="M12.6667 4.27337L11.7267 3.33337L8.00004 7.06004L4.27337 3.33337L3.33337 4.27337L7.06004 8.00004L3.33337 11.7267L4.27337 12.6667L8.00004 8.94004L11.7267 12.6667L12.6667 11.7267L8.94004 8.00004L12.6667 4.27337Z"
+							fill="#f00"
+						/>
+					</svg>
+				</div>
+			</div>
+		`;
+		total += cartProduct.price;
+	});
+	document.getElementById("cart-total").innerText = total;
+};
+
+// Handle Delete From Cart
+const handleDeleteFromCart = (productId) => {
+	const cartAfterDelete = cartProducts.filter(
+		(cartProduct) => cartProduct.id !== productId
+	);
+	cartProducts = cartAfterDelete;
+	showCartItems(cartProducts);
 };
 
 loadPlantByCategory(0);
